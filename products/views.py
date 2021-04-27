fea1a81a9b386e696057f30e30e66d038902eba3
Alicepinch <alicepinch@hotmail.co.uk>
2021-04-27 16:32:23 +0000

    Search by category and occasion functionality added

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category, Occasion
 
 # Create your views here.
 
@@ -13,20 +13,34 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
+    occasions = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'occasion' in request.GET:
+            occasions = request.GET['occasion'].split(',')
+            products = products.filter(occasion__name__in=occasions)
+            occasions = Occasion.objects.filter(name__in=occasions)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter anything!")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query) 
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_occasion': occasions
     }
 
     return render(request, 'products/products.html', context)
