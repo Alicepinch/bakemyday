ab77aaa8ff392290c40919d6efaa83afdd95922f
Alicepinch <alicepinch@hotmail.co.uk>
2021-05-06 11:37:39 +0000

    Product size logic added

--- bag/views.py
+++ bag/views.py
@@ -12,13 +12,29 @@ def add_item(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    cupcakesize = None
+    cakesize = None
+    size = None
+    if 'cake_size' or 'cupcake_size' in request.POST:
+        cakesize = request.POST['cake_size']
+        cupcakesize = request.POST['cupcake_size']
+        size = cupcakesize or cakesize
     bag = request.session.get('bag', {})
 
-    # Increases quantity of product if item is in bag already
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['cakes_by_size'].keys():
+                bag[item_id]['cakes_by_size'][size] += quantity
+            else:
+                bag[item_id]['cakes_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'cakes_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+    # Increases quantity of product if item is in bag already
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
