61b1ca185cffd1e7fde528d4461d4c00c8ac3a81
Alicepinch <alicepinch@hotmail.co.uk>
2021-05-13 15:17:38 +0000

    Remove view added and JS added to bag page

--- bag/urls.py
+++ bag/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>', views.add_item, name='add_item'),
-    path('adjust/<item_id>', views.adjust_item, name='adjust_item')
+    path('adjust/<item_id>', views.adjust_item, name='adjust_item'),
+    path('remove/<item_id>', views.remove_item, name='remove_item')
 ]
