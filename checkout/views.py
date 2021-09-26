4f52aafcf766e0ba3c8909390efa0e9461f56d01
Alicepinch <alicepinch@hotmail.co.uk>
2021-09-26 09:19:23 +0000

    Stripe card element, intial CSS and JS added for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IVePKGyadVnHladJwYAjkC2VPCIMGgmQxUIYcNp2czArC5qjsWCXLTbTNQu3dIxW51hyge3obgU6ksI1bnRoZw600KtaUrskI',
     }
 
     return render(request, template, context)
