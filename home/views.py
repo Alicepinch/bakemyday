6e2bb2d6ff267ab3ab57efde9b61c58e7c7ecab8
Alicepinch <alicepinch@hotmail.co.uk>
2021-11-18 18:03:57 +0000

    Contact us and About us pages added

--- home/views.py
+++ home/views.py
@@ -8,3 +8,17 @@ def index(request):
     View to return index page
     """
     return render(request, 'home/index.html')
+
+
+def contact_us(request):
+    """
+    View to return contact us page
+    """
+    return render(request, 'home/contact-us.html')
+
+
+def about_us(request):
+    """
+    View to return about us page
+    """
+    return render(request, 'home/about-us.html')
