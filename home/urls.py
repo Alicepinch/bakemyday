fd8468d11b7e715d6dad9017e63c8643d1abbcee
Alicepinch <alicepinch@hotmail.co.uk>
2021-11-22 21:24:11 +0000

    Contact us page removed from home app

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('contact-us/', views.contact_us, name='contact_us'),
     path('about-us/', views.about_us, name='about_us'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,20 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def index(request):
     """
     View to return index page
     """
     return render(request, 'home/index.html')
-
-
-def contact_us(request):
-    """
-    View to return contact us page
-    """
-    return render(request, 'home/contact-us.html')
 
 
 def about_us(request):
