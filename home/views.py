140327f7ba8f30f0a2618f65a2a524cd39b37729
Alicepinch <alicepinch@hotmail.co.uk>
2021-11-24 15:45:27 +0000

    About us removed from homepage views, urls and footer

--- home/views.py
+++ home/views.py
@@ -6,10 +6,3 @@ def index(request):
     View to return index page
     """
     return render(request, 'home/index.html')
-
-
-def about_us(request):
-    """
-    View to return about us page
-    """
-    return render(request, 'home/about-us.html')
